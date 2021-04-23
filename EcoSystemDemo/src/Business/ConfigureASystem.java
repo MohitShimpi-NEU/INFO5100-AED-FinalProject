b164f46dd8b093d0075f32f997ac8972caa29b28
MohitShimpi-NEU <shimpi.m@northeastern.edu>
2021-04-23 00:26:10 -0400

    Branch Testing

--- EcoSystemDemo/src/Business/ConfigureASystem.java
+++ EcoSystemDemo/src/Business/ConfigureASystem.java
@@ -14,13 +14,7 @@ public class ConfigureASystem {
         
         EcoSystem system = EcoSystem.getInstance();
         
-        //Create a network
-        //create an enterprise
-        //initialize some organizations
-        //have some employees 
-        //create user account
-        
-        
+
         Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
         
         UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
